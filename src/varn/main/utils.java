135bba6f3b18582f87a166ae197af7d3edc41ba1
varn <varnbu@gmail.com>
2019-12-09 16:46:26 +0800

    枚举的使用

---
+++ src/varn/main/utils.java
@@ -0,0 +1,12 @@
+package varn.main;
+
+public class utils {
+    public static void main(String[] args) {
+//        常用工具类 Math 浮点数不同的平台计算结果可能不一样
+//        StrictMath 保证给各个平台计算结果一致 提供的方法几乎同 Math
+//        Random 伪随机数
+//         SecureRandom  真随机数
+
+
+    }
+}

631c312a10b28c75fb5db0d2a9ad1df18dc2a4d8
varn <varnbu@gmail.com>
2019-12-09 11:26:05 +0800

    string

--- src/varn/main/StringLearn/StringLearn.java
+++ src/varn/main/StringLearn/StringLearn.java
@@ -48,7 +48,7 @@ public class StringLearn {
         String[] sarr = ss.split(",");
         System.out.println(Arrays.toString(sarr));
 //      拼接字符串
-        System.out.println(String.join("-",sarr));
+        System.out.println(String.join("-", sarr));
 
 //      类型转换
         String.valueOf(123);
@@ -56,6 +56,14 @@ public class StringLearn {
 //      转换为 char[]
         char[] cs = "hello".toCharArray();
         String sss = new String(cs);
+//      java 的 String 与 char 在内存中总以 Unicode 编码表示
+//      UTF-8 是一种变长编码  把固定长度的 Unicode 编码编程 1~4个字节的 变长编码
+
+//       StringBuilder 标准库中提供的高效拼接字符串的可变对象 预分配缓冲区
+//       StringBuffer  早期Java的的一个StringBuilder的线程安全版本 通过同步来保证多个线程的操作StringBuffer 是安全的
+//        但是同步会带来执行效率下降 现在很少使用
+
+//      StringJoiner 用户拼接数组字符串  类似于 String.join  但是 可以多两个参数用户指定字符串的开头结尾
 
     }
 }

2be23a8e8d6db06d940bcdd796cda7c445af24f0
varn <varnbu@gmail.com>
2019-12-10 10:52:57 +0800

    jvm 自带 logger

--- src/varn/main/utils.java
+++ src/varn/main/utils.java
@@ -1,12 +1,17 @@
 package varn.main;
 
+import java.util.logging.Logger;
+
 public class utils {
     public static void main(String[] args) {
 //        常用工具类 Math 浮点数不同的平台计算结果可能不一样
 //        StrictMath 保证给各个平台计算结果一致 提供的方法几乎同 Math
 //        Random 伪随机数
 //         SecureRandom  真随机数
-
-
+        Logger logger = Logger.getGlobal();
+        logger.info("sdf");
+        logger.warning("警告");
+        logger.fine("ignored");
+        logger.severe("process will be terminated");
     }
 }
